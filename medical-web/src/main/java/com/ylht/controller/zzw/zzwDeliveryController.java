c1034493809cd64ebb0bf8c9276c871e9b8c53bf
zzw <1101133895@qq.com>
2020-06-28 08:40:57 +0800

    更新

--- medical-web/src/main/java/com/ylht/controller/zzw/zzwDeliveryController.java
+++ medical-web/src/main/java/com/ylht/controller/zzw/zzwDeliveryController.java
@@ -1,8 +1,10 @@
 package com.ylht.controller.zzw;
 
 import com.ylht.pojo.Delivery;
+import com.ylht.pojo.Mpuser;
 import com.ylht.service.DeliveryService;
 import com.ylht.service.DeliverysService;
+import com.ylht.service.MpuserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +13,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("zDelivery")
-public class DeliveryController {
+public class ZzwDeliveryController {
     @Autowired
     private DeliveryService deliveryService;
+    @Autowired
+    private MpuserService mpuserService;
 
     @RequestMapping("getDelivery")
     public List<Delivery> getDelivery(Delivery delivery){
@@ -26,6 +30,19 @@ public class DeliveryController {
     @RequestMapping("delDelivery")
     public boolean delDelivery(Delivery delivery){
         Integer result=deliveryService.del(delivery);
+        if (result>0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    @RequestMapping("getMpuser")
+    public List<Mpuser> getMpuser(){
+        return mpuserService.getAll();
+    }
+    @RequestMapping("updDelivery")
+    public boolean updDelivery(Delivery delivery){
+        Integer result= deliveryService.upd(delivery);
         if (result>0){
             return true;
         }else {
