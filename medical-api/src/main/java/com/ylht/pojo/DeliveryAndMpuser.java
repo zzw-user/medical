f63cb14245cd2b5632dd056d744faf42496c589c
hh <2624605241@qq.com>
2020-06-29 11:21:22 +0800

    zd功能提交629

---
+++ medical-api/src/main/java/com/ylht/pojo/DeliveryAndMpuser.java
@@ -0,0 +1,29 @@
+package com.ylht.pojo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)//链式写法
+public class DeliveryAndMpuser implements Serializable {
+    private Integer did;
+    private Integer operator;
+    private String phone;
+    private String address;
+    private Integer aftertype;
+    private String coding;
+    @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    private Date deliverytime;
+    private String realname;
+    private String mname;
+}
---
+++ medical-api/src/main/java/com/ylht/pojo/InstallationRecordEvaluation.java
@@ -0,0 +1,28 @@
+package com.ylht.pojo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)//链式写法
+public class InstallationRecordEvaluation {
+    private Integer rid;
+    private Integer operator;
+    private String address;
+    private String beforeinstallation;
+    private String afterinstallation;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date installationtime;
+    private String assess;
+    private Integer status;
+    private String coding;
+    private String realname;
+}
