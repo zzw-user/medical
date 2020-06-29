31abf5ec225d4c5d25c0fe59fe1bd837cf37662c
hh <2624605241@qq.com>
2020-06-29 11:38:14 +0800

    ASSS

---
+++ medical-serviceImpl/src/main/java/com/ylht/serviceImplzxz/InstallationRecordEvaluationServiceImpl.java
@@ -0,0 +1,34 @@
+package com.ylht.serviceImplzxz;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.ylht.mapperzxz.InstallationRecordEvaluationMapper;
+import com.ylht.pojo.InstallationRecordEvaluation;
+import com.ylht.pojo.Installrecord;
+import com.ylht.servicezxz.InstallationRecordEvaluationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InstallationRecordEvaluationServiceImpl implements InstallationRecordEvaluationService {
+   @Autowired
+   private InstallationRecordEvaluationMapper installationRecordEvaluationMapper;
+
+
+    public List<InstallationRecordEvaluation> getInstallationRecordEvaluation(String address, Integer status, String coding, String realname, Integer pageNo, Integer pageSize) {
+        return installationRecordEvaluationMapper.getInstallationRecordEvaluation(address, status, coding, realname, pageNo, pageSize);
+    }
+
+    public PageInfo<InstallationRecordEvaluation> pageInstallationRecordEvaluation(String address, Integer status, String coding, String realname, Integer pageNo, Integer pageSize) {
+        PageHelper.startPage(pageNo,pageSize);
+        List<InstallationRecordEvaluation> list = installationRecordEvaluationMapper.getInstallationRecordEvaluation(address, status, coding, realname, pageNo, pageSize);
+        PageInfo<InstallationRecordEvaluation> pageInfo = new PageInfo<InstallationRecordEvaluation>(list);
+        return pageInfo;
+    }
+
+    public int updateInstallrecordByRid(Installrecord installrecord) {
+        return installationRecordEvaluationMapper.updateInstallrecordByRid(installrecord);
+    }
+}
